6e9b1b6bdba4d45421aec1d1a1b9beeae4e331fe
sanghk32 <sanghk32@naver.com>
2024-08-27 06:54:40 +0900

    commit main

---
+++ src/main/java/com/sh/studyyunghan/StudyYunghanApplication.java
@@ -0,0 +1,13 @@
+package com.sh.studyyunghan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyYunghanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyYunghanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sh/studyyunghan/StudyYunghanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sh.studyyunghan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyYunghanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
